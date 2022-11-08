e6806a83bee6538d4886325b311e6d3d7f59b09e
Adhya Rajaram <ar3620@aube.doc.ic.ac.uk>
2022-11-08 15:42:34 +0000

    Debug syscall code

--- src/userprog/syscall.c
+++ src/userprog/syscall.c
@@ -25,32 +25,6 @@ syscall_init (void)
   intr_register_int (0x30, 3, INTR_ON, syscall_handler, "syscall");
 }
 
-static void
-syscall_handler (struct intr_frame *f) 
-{
-  if (!FILESYS_ACQUIRE)
-  {
-    lock_init(&file_sys_lock);
-    FILESYS_ACQUIRE = true;
-  }
-
-  int esp = get_page_ptr((const void *) f->esp);
-  void (*sys_functions_arr[])(struct intr_frame *f) = {sys_halt,sys_exit, sys_exec, sys_wait, sys_create, sys_remove, sys_open,
-  sys_filesize, sys_read, sys_write, sys_seek, sys_tell, sys_close};
-  
-  /* need to find a way to see which functions return a value and set f->eax to it */
-  /* funcs returning a value are sys_exec, sys_wait, sys_create, sys_remove, 
-  sys_open, sys_filesize, sys_read, sys_write, sys_tell */
-
-  if ((* (int *) esp) < SYS_INUMBER)
-  { /* checks that esp is an enum */
-    (*sys_functions_arr[(* (int *) esp])(f) 
-  }
-  
-  printf ("system call!\n");
-  thread_exit ();
-}
-
 /* Reads a byte at user virtual address UADDR.
    UADDR must be below PHYS_BASE.
    Returns the byte value if successful, -1 if a segfault
@@ -121,31 +95,32 @@ get_stack_args (struct intr_frame *f,  int *args, int *num_of_args)
 
 
 /* gets page pointer using virtual address */
-int get_page_ptr(const void *vaddr){
+int get_page_ptr(struct intr_frame *f)
+{
+  const void *vaddr = (const void *) f->esp;
   void *ptr = pagedir_get_page(thread_current()->pagedir, vaddr);
   if (!ptr)
   {
-    syscall_exit(ERROR);
+    sys_exit();
   }
   return (int) ptr;
-
+}
 
 /* Terminates Pintos by calling 
    shutdown_power_off() 
 */
 void sys_halt (void){
-  int args[MAX_ARGS];
-  int esp = get_page_ptr((const void *) f->esp);
+  /* TO DO */
 }
 
 /* Terminates the current user program and sends exit status to kernel.
    A status of 0 is a success.
 */
 void sys_exit (struct intr_frame *f){
-  get_stack_args (f, 1, &args[0]);
   int args[MAX_ARGS];
-  int esp = get_page_ptr((const void *) f->esp);
-  int status = arg[0];
+  int esp = get_page_ptr(f);
+  get_stack_args (f, &args[0], 1);
+  int status = args[0];
 }
 
 /* Runs executable whose name is given in the command line, 
@@ -155,13 +130,13 @@ void
 sys_exec (struct intr_frame *f)
 {
   int args[MAX_ARGS];
-  int esp = get_page_ptr((const void *) f->esp);
-  get_stack_args(f, 1, &args[0]);
+  int esp = get_page_ptr(f);
+  get_stack_args(f, &args[0], 1);
 
   /* check that arg[0] is valid */
   /* validate_str((const void*) arg[0])*/
   /* get page pointer */
-  args[0] = get_page_ptr ((const void *)args[0]);
+  args[0] = get_page_ptr (f);
   /* pid_t exec (const char *cmd_line) */
   const char *cmd_line = (const char *) args[0];
   /* formerly passed into the function as an argument */
@@ -183,10 +158,10 @@ sys_exec (struct intr_frame *f)
 void sys_wait (struct intr_frame *f)
 {
   int args[MAX_ARGS];
-  int esp = get_page_ptr((const void *) f->esp);
-  get_stack_args(f, 1, &args[0]);
+  int esp = get_page_ptr(f);
+  get_stack_args(f, &args[0], 1);
       /* int wait (pid t pid) */
-  pid_t pid = arg[0]; /* previously passed as arg */
+  pid_t pid = args[0]; /* previously passed as arg */
   /* change the eax value (below) once sys_wait has been implemented */
   f->eax = -1;
 }
@@ -196,16 +171,16 @@ void sys_wait (struct intr_frame *f)
 void sys_create (struct intr_frame *f)
 {
   int args[MAX_ARGS];
-  int esp = get_page_ptr((const void *) f->esp);
+  int esp = get_page_ptr(f);
 
-  get_stack_args(f, 2, &args[0]);
+  get_stack_args(f, &args[0], 1);
 
   /* check that arg[0] is valid */
   /* validate_str((const void*) arg[0])*/
   args[0] = get_page_ptr((const void *)args[0]);
   /* bool create (const char *file, unsigned initial_size) */
   const char *file = (const char *) args[0];
-  unsigned initial_size = (unsigned) args[1]);
+  unsigned initial_size = (unsigned) args[1];
   /* change eax val (below) once sys_create has been completed */
   f->eax = false;
 }
@@ -217,12 +192,12 @@ void sys_create (struct intr_frame *f)
 void sys_remove (struct intr_frame *f)
 {
   int args[MAX_ARGS];
-  int esp = get_page_ptr((const void *) f->esp);
+  int esp = get_page_ptr(f);
 
-  get_stack_args(f, 1, &args[0]);
+  get_stack_args(f, &args[0], 1);
   /* check that arg[0] is valid */
   /* validate_str((const void*) arg[0])*/
-  args[0] = get_page_ptr((const void *)args[0]);
+  args[0] = get_page_ptr(f);
   /* bool remove (const char *file) */
   const char *file = (const char *) args[0];
   /* change eax val (below) once sys_remove has been completed */
@@ -232,16 +207,16 @@ void sys_remove (struct intr_frame *f)
 /* Tries to open the file.
    If successful, the function returns -1.
    Otherwise, it returns the file descriptor. */
-void sys_open (struct intr_frame *f, const char *file)
+void sys_open (struct intr_frame *f)
 {
   int args[MAX_ARGS];
-  int esp = get_page_ptr((const void *) f->esp);
+  int esp = get_page_ptr(f);
 
-  get_stack_args(f, 1, &args[0]);
+  get_stack_args(f, &args[0], 1);
 
   /* check that arg[0] is valid */
   /* validate_str((const void*) arg[0])*/
-  args[0] = get_page_ptr((const void *)args[0]);
+  args[0] = get_page_ptr(f);
   /* int open (const char *file) */
   const char *file = (const char *) args[0];
   /* change eax val (below) once sys_open has been completed */
@@ -249,47 +224,49 @@ void sys_open (struct intr_frame *f, const char *file)
 }
 
 /* Returns the size, in bytes, of the file open as fd.*/
-void sys_filesize (struct intr_frame *f, int fd)
+void sys_filesize (struct intr_frame *f)
 {
   int args[MAX_ARGS];
   int esp = get_page_ptr((const void *) f->esp);
 
-  get_stack_args(f, 1, &args[0]);
+  get_stack_args(f, &args[0], 1);
   /* int filesize (int fd) */
   int fd = args[0];
   /* change eax val (below) once sys_filesize has been completed */
   f->eax = 0;
-  }
+}
 
 /* Reads size bytes from the file open as fd into buffer */
 void sys_read (struct intr_frame *f)
 {
   int args[MAX_ARGS];
-  int esp = get_page_ptr((const void *) f->esp);
+  int esp = get_page_ptr(f);
 
-  get_stack_args(f, 3, &args[0]);
+  get_stack_args(f, &args[0], 3);
   /* check that buffer is valid */
   /* validate_buffer((const void *) arg[1], (unsigned) arg[2])*/
-  args[1] = get_page_ptr((const void *)args[0]);
+  args[1] = get_page_ptr(f);
   /* int read (int fd, void *buffer, unsigned size) */
-  int fd = args[0];
+
+  /* Don't think I need the below
+  /*int fd = args[0];
   void *buffer = (void *) args[1];
-  unsigned size = (unsigned) args[2];
+  unsigned size = (unsigned) args[2];*/
    /* change eax val (below) once sys_remove has been completed */
   f->eax = 0;
 }
 
 /* Writes size bytes from buffer to the open file fd. Returns the number of bytes actually
    written, which may be less than size if some bytes could not be written */
-void sys_write (struct intr_frame *f, int fd, , unsigned size)
+void sys_write (struct intr_frame *f)
 {
   int args[MAX_ARGS];
-  int esp = get_page_ptr((const void *) f->esp);
+  int esp = get_page_ptr(f);
 
-  get_stack_args(f, 3, &args[0]);
+  get_stack_args(f, &args[0], 3);
   /* check that buffer is valid */
   /* validate_buffer((const void *) arg[1], (unsigned) arg[2])*/
-  args[1] = get_page_ptr((const void *)arg[0]);
+  args[1] = get_page_ptr(f);
   /* int write (int fd, const void *buffer, unsigned size) */
   int fd = args[0];
   const void *buffer = (const void *) args[1];
@@ -302,19 +279,19 @@ void sys_write (struct intr_frame *f, int fd, , unsigned size)
 from the beginning of the file. */
 void sys_seek (struct intr_frame *f) {
   int args[MAX_ARGS];
-  int esp = get_page_ptr((const void *) f->esp);
+  int esp = get_page_ptr(f);
 
-  get_stack_args(f, 2, &args[0]);
+  get_stack_args(f, &args[0], 2);
   /* void seek (int fd, unsigned position) */
   int fd = args[0];
   unsigned position = (unsigned) args[1];
 }
 
 /* Returns the position of the next byte to be read or written in open file fd */
-void sys_tell (struct intr_frame *f, int fd)
+void sys_tell (struct intr_frame *f)
 {
   int args[MAX_ARGS];
-  int esp = get_page_ptr((const void *) f->esp);
+  int esp = get_page_ptr(f);
   get_stack_args(f, 1, &args[0]);
   /* unsigned tell (int fd) */
   int fd = args[0];
@@ -323,13 +300,37 @@ void sys_tell (struct intr_frame *f, int fd)
 }
 
 /* Closes file descriptor fd.*/
-void sys_close (struct intr_frame *f, int fd){
+void sys_close (struct intr_frame *f){
   int args[MAX_ARGS];
-  int esp = get_page_ptr((const void *) f->esp);
+  int esp = get_page_ptr(f);
 
   get_stack_args(f, 1, &args[0]);
   /* void close (int fd) */
   int fd = args[0];
 }
 
+static void
+syscall_handler (struct intr_frame *f)
+{
+  if (!FILESYS_LOCK_ACQUIRE)
+  {
+    lock_init(&file_sys_lock);
+    FILESYS_LOCK_ACQUIRE = true;
+  }
 
+  int esp = get_page_ptr(f);
+  void (*sys_functions_arr[])(struct intr_frame *f) = {sys_halt,sys_exit, sys_exec, sys_wait, sys_create, sys_remove, sys_open,
+  sys_filesize, sys_read, sys_write, sys_seek, sys_tell, sys_close};
+
+  /* need to find a way to see which functions return a value and set f->eax to it */
+  /* funcs returning a value are sys_exec, sys_wait, sys_create, sys_remove,
+  sys_open, sys_filesize, sys_read, sys_write, sys_tell */
+
+  if ((* (int *) esp) < SYS_INUMBER)
+  { /* checks that esp is an enum */
+    (*sys_functions_arr[(* (int *) esp)])(f);
+  }
+
+  printf ("system call!\n");
+  thread_exit ();
+}
