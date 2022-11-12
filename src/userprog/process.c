7d9098e31010b48a9c4ed5a5b3f9645bdbb783ae
Sophia Rekers <ser121@ic.ac.uk>
2022-11-12 22:22:27 +0000

    Update wait_handler tid after thread creation

--- src/userprog/process.c
+++ src/userprog/process.c
@@ -58,6 +58,7 @@ process_execute (const char *file_name)
 
   /* Create a new thread to execute FILE_NAME. */
   tid = thread_create (file_name, PRI_DEFAULT, start_process, fn_copy);
+  wh->tid = tid;
   if (tid == TID_ERROR)
     palloc_free_page (fn_copy); 
   return tid;
