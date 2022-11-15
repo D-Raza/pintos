2d2356d89737cca08f83e3dd95e56f0e18f677a5
Alptug Polatkan <aap221@texel07.doc.ic.ac.uk>
2022-11-15 19:08:37 +0000

    Create the struct-based mapping between file descriptor and file struct

--- src/userprog/process.h
+++ src/userprog/process.h
@@ -1,6 +1,7 @@
 #ifndef USERPROG_PROCESS_H
 #define USERPROG_PROCESS_H
 
+#include "filesys/file.c"
 #include "threads/thread.h"
 #include "threads/synch.h"
 
@@ -27,4 +28,11 @@ struct process_start_aux
   void *filename;
   struct wait_handler* wait_handler;
 };
+
+struct fd_to_file_mapping {
+  int fd;                        /* file descriptor */
+  struct file file_struct;       /* file struct corresponding to fd */
+  struct list_elem elem;         /* List element */
+};
+
 #endif /* userprog/process.h */
