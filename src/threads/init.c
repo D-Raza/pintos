a16ab1b7ae6c9df36ed7b9be4466a69f0ab7feb0
D-Raza <danish281202@gmail.com>
2022-11-24 20:34:57 +0000

    [ar3620, dr421] Initialise frame table

--- src/threads/init.c
+++ src/threads/init.c
@@ -37,6 +37,11 @@
 #include "filesys/filesys.h"
 #include "filesys/fsutil.h"
 #endif
+#ifdef VM
+#include "vm/frame.h"
+#include "vm/page.h"
+#include "vm/swap.h"
+#endif
 
 /* Page directory with kernel mappings only. */
 uint32_t *init_page_dir;
@@ -98,6 +103,10 @@ main (void)
   palloc_init (user_page_limit);
   malloc_init ();
   paging_init ();
+
+#ifdef VM
+  frame_init ();
+#endif
 
   /* Segmentation. */
 #ifdef USERPROG
