00bc5ad111acd15fdfa159a8b4fa5d06527134d2
Alptug Polatkan <aap221@texel07.doc.ic.ac.uk>
2022-11-25 16:32:30 +0000

    [ar3620, app221] Add list of page table references in frame table entry and struct for page table references

--- src/vm/frame.h
+++ src/vm/frame.h
@@ -13,13 +13,19 @@
 struct frame_table_entry
 {
     void *kpage;                  /* Kernel virtual address of the page */
-    void *upage;                  /* User virtual address of the page */
-    uint32_t *pd;                  /* Page directory of the thread that owns the page */
+    void *upage;                  /* User virtual address of the page */   
+    struct list *page_table_refs;  /* User pages that refer to the same frame (aliases) */  
     struct hash_elem hash_elem;   /* Hash table element */
     struct list_elem list_elem;   /* List element for eviction */
-    bool evictable;               /* Whether the page is evictable */
+    bool evictable;               /* Whether the frame is evictable */
 };
 
+struct page_table_ref 
+{
+    void *pd;                     /* Page directory of the thread that owns the page */
+    void *page;                   /* User page corresponding to pd */
+    struct list_elem elem;        /* List Elem */
+};
 
 void frame_init (void);
 void frame_free (void *kpage);
