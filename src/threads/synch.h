78d76dd7c489fd479d5a1238f9ba6daa7e991fdd
ar3620 <ar3620@ic.ac.uk>
2022-12-08 13:55:35 +0000

    Add re-entrance to lock and refactor lock functions

--- src/threads/synch.h
+++ src/threads/synch.h
@@ -23,6 +23,7 @@ struct lock
     struct thread *holder;      /* Thread holding lock (for debugging). */
     struct semaphore semaphore; /* Binary semaphore controlling access. */
     struct list_elem elem;      /* List element for list of donors in thread. */
+    int count;			/* Re-entrance counter */
   };
 
 void lock_init (struct lock *);
